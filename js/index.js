3ea255d9a8d99ae8c867927b63cebe89bcb47379
Erlend Johnsen <96027411+johnsulf@users.noreply.github.com>
2023-03-01 15:31:10 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,18 +2,19 @@ const url = "https://pokeapi.co/api/v2/pokemon/";
 const pokemonCardsContainer = document.querySelector(".pokemon-cards");
 const loaderContainer = document.querySelector(".loader-container");
 
+pokemonCardsContainer.style.display = "none";
+
 async function fetchPokemon() {
   try {
     for (let i = 0; i < 151; i++) {
       const pokemonResponse = await fetch(url + `${i + 1}`);
       const pokemonDetails = await pokemonResponse.json();
 
-      loaderContainer.style.display = "none";
-
       const type = pokemonDetails.types[0].type.name;
       const color = setColorFromType(type);
 
-      const html = ` <div class="pokemon-card" style="background-color: ${color}${"90"};">
+      const html = `
+        <div class="pokemon-card" style="background-color: ${color}${"90"};">
           <div>
             <div class="top-row">
               <h3 class="name">${pokemonDetails.name}</h3>
@@ -23,9 +24,7 @@ async function fetchPokemon() {
               <img src="${pokemonDetails.sprites.front_default}">
             </div>
           </div>
-          <a class="more-link" href="details.html?id=${
-            pokemonDetails.id
-          }">More</a>
+          <a class="more-link" href="details.html?id=${pokemonDetails.id}">More</a>
           <p></p>
         </div>`;
 
@@ -33,6 +32,9 @@ async function fetchPokemon() {
     }
   } catch (e) {
     console.log(e);
+  } finally {
+    loaderContainer.style.display = "none";
+    pokemonCardsContainer.style.display = "flex";
   }
 }
 
